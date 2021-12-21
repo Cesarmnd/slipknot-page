752d44032ab646d6402248bf20f57839443412ab
César Niño <cesarn.davila@gmail.com>
2021-12-20 23:32:50 -0500

    Últimos cambios

--- scripts/code.js
+++ scripts/code.js
@@ -6,7 +6,261 @@ if (toastTrigger) {
         var toast = new bootstrap.Toast(toastLiveExample)
         toast.show()
     })
-}
+};
+
+var toastTrigger = document.getElementById('liveToastBtn1')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn2')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn3')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn4')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn5')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn6')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn7')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn8')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn9')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn10')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn11')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn12')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn13')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn14')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn15')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn16')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn17')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn18')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn19')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn20')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn21')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn22')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn23')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn24')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn25')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn26')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn27')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+var toastTrigger = document.getElementById('liveToastBtn28')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function () {
+        var toast = new bootstrap.Toast(toastLiveExample)
+        toast.show()
+    })
+};
+
+
 
 // Nav para resolución Mobile
 document.addEventListener("DOMContentLoaded", function(){
@@ -17,4 +271,333 @@ document.addEventListener("DOMContentLoaded", function(){
       e.stopPropagation();
       bsOffcanvas.toggle();
     });
-  });
+});
+
+  // Sumar productos para la sección Carrito
+  let carts = document.querySelectorAll('.add-cart');
+
+  let products = [
+    // Máscaras
+    {
+      item: "Shawn Crahan Mask",
+      tag: "clownmask",
+      price: 25,
+      inCart: 0,
+    },
+
+    {
+      item: "Craig Jones Mask",
+      tag: "craigmask",
+      price: 25,
+      inCart: 0,
+    },
+
+    {
+      item: "Mick Thomson Mask",
+      tag: "mickmask",
+      price: 25,
+      inCart: 0,
+    },
+
+    {
+      item: "Corey Taylor Mask",
+      tag: "coreymask",
+      price: 25,
+      inCart: 0,
+    },
+    
+    {
+      item: "Sid Wilson Mask",
+      tag: "sidmask",
+      price: 25,
+      inCart: 0,
+    },
+    
+    {
+      item: "Jim Root Mask",
+      tag: "jimmask",
+      price: 25,
+      inCart: 0,
+    },
+    
+    {
+      item: "Alessandro Venturella Mask",
+      tag: "vmanmask",
+      price: 25,
+      inCart: 0,
+    },
+    
+    {
+      item: "Jay Weignberg Mask",
+      tag: "jaymask",
+      price: 25,
+      inCart: 0,
+    },
+
+    {
+      item: "Michael Pfaff Mask",
+      tag: "tortillamask",
+      price: 25,
+      inCart: 0,
+    },
+
+    // Prendas
+    {
+      item: "Gray T-Shirt",
+      tag: "shirt",
+      price: 15,
+      inCart: 0,
+    },
+    
+    {
+      item: "Iowa T-shirt",
+      tag: "slipknotfront",
+      price: 15,
+      inCart: 0,
+    },
+    
+    {
+      item: "Iowa Hoodie",
+      tag: "SLIPKNOTIOWA20HOODIEFRONT",
+      price: 15,
+      inCart: 0,
+    },
+    
+    {
+      item: "Band Suit",
+      tag: "traje",
+      price: 15,
+      inCart: 0,
+    },
+    
+    {
+      item: "Black Hat",
+      tag: "sombreronegro",
+      price: 15,
+      inCart: 0,
+    },
+
+    {
+      item: "Band Hooder",
+      tag: "poleralogo",
+      price: 15,
+      inCart: 0,
+    },
+
+    // Polainas
+    {
+      item: "Shawn Crahan Neck",
+      tag: "CLOWNNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+
+    {
+      item: "Craig Jones Neck",
+      tag: "CRAIGNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+
+    {
+      item: "Mick Thomson Neck",
+      tag: "MICKNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+
+    {
+      item: "Corey Taylor Neck",
+      tag: "COREYNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+    
+    {
+      item: "Sid Wilson Neck",
+      tag: "SIDNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+    
+    {
+      item: "Jim Root Neck",
+      tag: "JIMNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+    
+    {
+      item: "Alessandro Venturella Neck",
+      tag: "V-MANNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+    
+    {
+      item: "Jay Weignberg Neck",
+      tag: "JAYNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+
+    {
+      item: "Michael Pfaff Neck",
+      tag: "NEWGUYNECKGAITER",
+      price: 20,
+      inCart: 0,
+    },
+
+    // Micelánea
+    {
+      item: "We Are not Your  Kind",
+      tag: "cd",
+      price: 8,
+      inCart: 0,
+    },
+    
+    {
+      item: "Iowa Jigsaw",
+      tag: "Slipknot_Iowa_3Dpuzzle",
+      price: 8,
+      inCart: 0,
+    },
+    
+    {
+      item: "Vol.3 Jigsaw",
+      tag: "Puzzle",
+      price: 8,
+      inCart: 0,
+    },
+    
+    {
+      item: "Black Freesbee",
+      tag: "freesbeeblack",
+      price: 8,
+      inCart: 0,
+    },
+    
+    {
+      item: "Red Freesbee",
+      tag: "frissbeered",
+      price: 8,
+      inCart: 0,
+    }  
+  ]
+
+  for (let i=0; i < carts.length; i++) { //Remover + 1
+    carts[i].addEventListener('click', function() {
+      cartNumbers(products[i]);
+      totalCost(products[i])
+    })
+  }
+
+  function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem("cartNumbers");
+
+    if (productNumbers) {
+      document.querySelector(".cart span").textContent = productNumbers;
+    }
+  }
+
+function cartNumbers (product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".cart span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
+  }
+
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  console.log("My cartItems are", cartItems);
+
+  if (cartItems != null) {
+    if(cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems, 
+        [product.tag]: product
+      }
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product
+    }
+  }
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+  let cartCost = localStorage.getItem('totalCost');
+
+  console.log("My cart cost is", cartCost);
+  console.log(typeof cartCost);
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + product.price)
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
+}
+
+function displayCart () {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  let productContainer = document.querySelector(".products");
+  let cartCost = localStorage.getItem('totalCost');
+
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = '';
+    Object.values(cartItems).map(item => {
+      productContainer.innerHTML +=  `
+      <div class="carts__product product">
+        <div class="carts__product-title">
+          <ion-icon name="close-outline"></ion-icon>
+          <img class="carts__img" src="../images/store/${item.tag}.png">
+          <span>${item.item}</span>
+        </div>
+        <div class="carts__price price">USD${item.price}.00</div>
+        <div class="carts__quantity cart__quantity--valor quantity">
+          <ion-icon class="carts__decrease decrease" name="caret-back"></ion-icon>
+          <span>${item.inCart}</span>
+          <ion-icon class="carts__increase increase" name="caret-forward"></ion-icon>
+        </div>
+        <div class="carts__total total">
+          USD${item.inCart * item.price}.00
+          <a class="carts__button" href="pago.html">
+            Pagar
+          </a>
+        </div>
+      </div>
+      `;
+    });
+
+    productContainer.innerHTML += `
+      <div class="carts__total-container">
+        <h4 class="carts__total-title">
+          Cart Total: 
+        </h4>
+        <h4 class"carts__total-amount">
+        USD ${cartCost}.00
+        </h4>
+      </div>
+    `  
+  }
+
+}
+
+onLoadCartNumbers();
+displayCart();
